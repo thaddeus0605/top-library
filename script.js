03ed4ef79e0f9e1b0825c4279c5a4d55e051472a
Thaddeus <tdronski@gmail.com>
2024-07-25 09:08:31 -0500

    Refactored code to put all functionality related to managing book data into one Class.

--- script.js
+++ script.js
@@ -5,55 +5,124 @@ const bookModal = document.getElementById('bookModal');
 const closeModal = document.getElementsByClassName('close')[0];
 const bookForm = document.getElementById('bookForm');
 
-function Book(title, author, pages, haveRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.haveRead = haveRead;
-}
+class Book {
+    constructor(title, author, pages, haveRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.haveRead = haveRead;
+    }
 
-function addBookToLibrary(bookTitle, bookAuthor, bookPages, haveRead) {
-    const newBook = new Book(bookTitle, bookAuthor, bookPages, haveRead);
-    myLibrary.push(newBook);
-    displayBook([newBook]); // Display only the new book
-}
+    toggleReadStatus() {
+        this.haveRead = !this.haveRead;
+    }
 
-function displayBook(library) {
-    for (let i = 0; i < library.length; i++) {
+    createBookElement() {
         const div = document.createElement('div');
         div.className = 'card';
-        bookList.appendChild(div);
 
         const h3 = document.createElement('h3');
         h3.className = 'bookTitleText';
-        h3.textContent = `${library[i].title}`;
+        h3.textContent = this.title;
         div.appendChild(h3);
 
         const author = document.createElement('p');
         author.className = 'authorText';
-        author.textContent = `Author: ${library[i].author}`;
+        author.textContent = `Author: ${this.author}`;
         div.appendChild(author);
 
         const pages = document.createElement('p');
         pages.className = 'pagesText';
-        pages.textContent = `Pages: ${library[i].pages}`;
+        pages.textContent = `Pages: ${this.pages}`;
         div.appendChild(pages);
+
+        const readStatusBtn = document.createElement('button');
+        readStatusBtn.textContent = this.haveRead ? 'Read' : 'Not Read';
+        readStatusBtn.className = this.haveRead ? 'readStatusBtn read' : 'readStatusBtn notRead';
+        div.appendChild(readStatusBtn);
+
+        readStatusBtn.addEventListener('click', () => {
+            this.toggleReadStatus();
+            readStatusBtn.textContent = this.haveRead ? 'Read' : 'Not Read';
+            readStatusBtn.className = this.haveRead ? 'readStatusBtn read' : 'readStatusBtn notRead';
+        });
 
         const deleteBtn = document.createElement('button');
         deleteBtn.textContent = 'Delete';
         deleteBtn.className = 'deleteBtn';
         div.appendChild(deleteBtn);
 
-
         deleteBtn.addEventListener('click', () => {
-            const index = myLibrary.indexOf(library[i]);
-            if(index !== -1) {
+            const index = myLibrary.indexOf(this);
+            if (index !== -1) {
                 myLibrary.splice(index, 1);
                 div.remove();
             }
         });
+
+        return div;
+    }
+
+    addToLibrary() { 
+        myLibrary.push(this);
+        const bookElement = this.createBookElement();
+        bookList.appendChild(bookElement);
     }
 }
+
+function addBookToLibrary(bookTitle, bookAuthor, bookPages, haveRead) {
+    const newBook = new Book(bookTitle, bookAuthor, bookPages, haveRead);
+    newBook.addToLibrary();
+}
+
+// function displayBook(library) {
+//     for (let i = 0; i < library.length; i++) {
+//         const div = document.createElement('div');
+//         div.className = 'card';
+//         bookList.appendChild(div);
+
+//         const h3 = document.createElement('h3');
+//         h3.className = 'bookTitleText';
+//         h3.textContent = `${library[i].title}`;
+//         div.appendChild(h3);
+
+//         const author = document.createElement('p');
+//         author.className = 'authorText';
+//         author.textContent = `Author: ${library[i].author}`;
+//         div.appendChild(author);
+
+//         const pages = document.createElement('p');
+//         pages.className = 'pagesText';
+//         pages.textContent = `Pages: ${library[i].pages}`;
+//         div.appendChild(pages);
+
+//         const readStatusBtn = document.createElement('button');
+//         readStatusBtn.textContent = library[i].haveRead ? 'Read' : 'Not Read';
+//         readStatusBtn.className = library[i].haveRead ? 'readStatusBtn read' : 'readStatusBtn notRead';
+//         div.appendChild(readStatusBtn);
+
+//         readStatusBtn.addEventListener('click', () => {
+//             library[i].haveRead = !library[i].haveRead;
+//             readStatusBtn.textContent = library[i].haveRead ? 'Read' : 'Not Read';
+//             readStatusBtn.className = library[i].haveRead ? 'readStatusBtn read' : 'readStatusBtn notRead';
+     
+//         })
+
+//         const deleteBtn = document.createElement('button');
+//         deleteBtn.textContent = 'Delete';
+//         deleteBtn.className = 'deleteBtn';
+//         div.appendChild(deleteBtn);
+
+
+//         deleteBtn.addEventListener('click', () => {
+//             const index = myLibrary.indexOf(library[i]);
+//             if(index !== -1) {
+//                 myLibrary.splice(index, 1);
+//                 div.remove();
+//             }
+//         });
+//     }
+// }
 
 // Open the modal
 addBookBtn.onclick = function() {
@@ -85,4 +154,7 @@ bookForm.onsubmit = function(event) {
 }
 
 // Display initial books (if any)
-displayBook(myLibrary);
+myLibrary.forEach(book => {
+    const bookElement = book.createBookElement();
+    bookList.appendChild(bookElement);
+});
